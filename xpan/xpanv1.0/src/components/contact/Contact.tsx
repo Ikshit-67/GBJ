80dc134b4590958406dee23e7a9aea9364f4c3dc
Ikshit C <109130289+Ikshit-67@users.noreply.github.com>
2023-01-14 23:00:57 +0530

    almost done

--- xpan/xpanv1.0/src/components/contact/Contact.tsx
+++ xpan/xpanv1.0/src/components/contact/Contact.tsx
@@ -1,10 +1,11 @@
-import { Button, TextField } from '@mui/material';
+import { Button, CircularProgress, TextField } from '@mui/material';
 import React, { useState } from 'react';
 import Styles from "./Contact.module.scss";
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 import Image from 'next/image';
 
 const Contact = () => {
+  const [loading, setLoading] = useState(false)
   const [userDetails, setUserdetails] = useState({
     name:"",
     email:"",
@@ -13,6 +14,7 @@ const Contact = () => {
 
 const submit = async (e:any)=>{
   e.preventDefault();
+  setLoading(true)
   try {
     const config: AxiosRequestConfig = {
       url: `/api/contact`,
@@ -20,9 +22,15 @@ const submit = async (e:any)=>{
       data: userDetails,
     };
     const res = await axios.request(config);
+    if (res.data.success){
+      alert("Thank you for your submission, your message has been sent successfully !")
+    } else {
+      alert("Oops, something went wrong !")
+    }
   } catch (error) {
     console.log(error);
-  }
+  } 
+  setLoading(false)
 }
 
   return (
@@ -38,7 +46,7 @@ const submit = async (e:any)=>{
     <TextField fullWidth required  type={"text"} label={"Your name"} size={"small"} variant={"outlined"} value={userDetails.name} onChange={(e)=>{setUserdetails((prev)=>({...prev, name:e.target.value}))}}/>
     <TextField fullWidth required type={"email"} label={"Your email"} size={"small"} variant={"outlined"} value={userDetails.email}  onChange={(e)=>{setUserdetails((prev)=>({...prev, email:e.target.value}))}}/>
     <TextField fullWidth required type={"text"} multiline minRows={5} label={"Message"} size={"small"} variant={"outlined"} value={userDetails.message} onChange={(e)=>{setUserdetails((prev)=>({...prev, message:e.target.value}))}}/>
-    <Button type='submit'>Send</Button>
+    <Button type='submit'>{loading ? <CircularProgress style={{color:"#fff", width:"25px", height:"25px"}} /> : "Send"}</Button>
     </form>
     </div>
 
--- xpan/xpanv1.0/src/pages/api/contact.ts
+++ xpan/xpanv1.0/src/pages/api/contact.ts
@@ -3,10 +3,10 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   port: 465, // true for 465, false for other ports
-  host: process.env.SMTP_HOST,
+  host: "smtp.gmail.com",
   auth: {
-    user: process.env.SMTP_EMAIL,
-    pass: process.env.SMTP_PASSWORD,
+    user: "kanjikards.app@gmail.com",
+    pass: "ticxsbhjnawofydz",
   },
   secure: true,
 });
@@ -18,20 +18,23 @@ export default async function handleFeedback(
   try {
     console.log(req.body);
     
-    // const { method } = req;
-    // if (method === "OPTIONS") {
-    //   return res.status(200).send("ok");
-    // }
-    // const mailData = {
-    //   from: "minimalist.ic@gmail.com", // sender address
-    //   to: `prtk.app.dev@gmail.com`, // list of receivers
-    //   subject: "Message from KanjiCards User!",
-    //  html: `<b>From: ${req.body.username}</b><br> <b>Email: ${req.body.email}</b><br/><p>Message: ${req.body.message}</p>`,
-    // };
-    // await transporter.sendMail(mailData, (err: any, info: any) => {
-    //   if (err) throw new Error("Error in sending verification email!");
-    //   console.log("Verification email sent successfully!");
-    // });
+    const { method } = req;
+    if (method === "OPTIONS") {
+      return res.status(200).send("ok");
+    }
+    const mailData = {
+      from: "minimalist.ic@gmail.com", // sender address
+      to: `prtk.app.dev@gmail.com`, // list of receivers
+      subject: "Message from KanjiCards User!",
+     html: `<b>From: ${req.body.name}</b><br> <b>Email: ${req.body.email}</b><br/><p>Message: ${req.body.message}</p>`,
+    };
+    console.log(req.body);
+    
+    await transporter.sendMail(mailData, (err: any, info: any) => {
+      if (err) throw new Error("Error in sending Message !");
+      console.log("Message sent successfully!");
+      res.status(200).json({success : true})
+    });
   } catch (error) {
     console.log(`Err at POST /sendFeedback`);
     res.status(400).json({ success: false, message: `${error}` });
