658cc4c365e4cba29c8611d26cf0e6668613ffc9
Ikshit C <109130289+Ikshit-67@users.noreply.github.com>
2023-01-14 19:27:32 +0530

    alpha done

---
+++ xpan/xpanv1.0/src/components/contact/Contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Styles from "./Contact.module.scss";
+
+const Contact = () => {
+  return (
+    <section className={Styles.contact}>
+
+    </section>
+  )
+}
+
+export default Contact
